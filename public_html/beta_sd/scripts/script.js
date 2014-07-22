b3f7304282381f0833a2b2e6ddf665a0400d730e
Kelly Yu <euriiko@gmail.com>
2014-07-22 18:50:11 -0400

    2 weeks worth of work. Probably should commit more :o

---
+++ public_html/beta_sd/scripts/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	/*based on the page, will have the page's title in the nav highlighted pink*/
+	$('nav > a:nth-child(' + pageNumber + ') > div').css('background-color', 'rgba(255, 191, 214, .5');
+	/*when using the hotkey nav (in the header), will also highlight the corresponding label in the nav*/
+	$('header > span').mouseenter(function() {
+		/*we access the index of the span that we are hovering over*/
+		// console.log('mouse entered');
+		// console.log($(this).index());
+		var letter = $(this).index() + 1;
+		if (letter != pageNumber) {
+			$('nav > a:nth-child(' + letter + ') > div').css('background-color', 'rgba(255, 191, 214, .3');
+		}
+		
+	});
+	$('header > span').mouseout(function() {
+		/*we access the index of the span that we are hovering over*/
+		// console.log('mouse left');
+		// console.log($(this).index());
+		var letter = $(this).index() + 1;
+		if (letter != pageNumber) {
+			$('nav > a:nth-child(' + letter + ') > div').removeAttr('style');
+		}
+	});
+});
