a8192c08406bf8a07cc099e8671ae1a8d730f5e3
Kelly Yu <euriiko@gmail.com>
2014-08-04 01:38:35 -0400

    adding photos, tweaked main design

--- public_html/beta_sd/scripts/script.js
+++ public_html/beta_sd/scripts/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	/*based on the page, will have the page's title in the nav highlighted pink*/
-	$('nav > a:nth-child(' + pageNumber + ') > div').css('background-color', 'rgba(255, 191, 214, .5');
+	$('nav > a:nth-child(' + pageNumber + ') > div').css('background-color', 'rgba(255, 255, 255, .9');
 	/*when using the hotkey nav (in the header), will also highlight the corresponding label in the nav*/
 	$('header > span').mouseenter(function() {
 		/*we access the index of the span that we are hovering over*/
@@ -8,7 +8,7 @@ $(document).ready(function() {
 		// console.log($(this).index());
 		var letter = $(this).index() + 1;
 		if (letter != pageNumber) {
-			$('nav > a:nth-child(' + letter + ') > div').css('background-color', 'rgba(255, 191, 214, .3');
+			$('nav > a:nth-child(' + letter + ') > div').css('background-color', 'rgba(255, 255, 255, .5');
 		}
 		
 	});
